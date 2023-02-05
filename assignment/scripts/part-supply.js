c8675ca8fb4f49b0eff17b4dac98db4bdee2d411
Steven Gangl <steven.h.gangl@gmail.com>
2023-02-05 15:30:01 -0600

    done with stretch

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -40,7 +40,7 @@ for (let x = 0; x <= 5; x++) {
         console.log('no change');
     }
     else if (supplyChanges[x] < 0) {
-        console.log('removed',supplyChanges[x], 'parts');
+        console.log('removed', supplyChanges[x], 'parts');
 
     }
 }
@@ -53,13 +53,13 @@ console.log('7. Showing supplyChanges with "for of" loop');
 
 for (let x of supplyChanges) {
     if (x > 0) {
-        console.log('added',x, 'parts');
+        console.log('added', x, 'parts');
     }
     else if (x === 0) {
         console.log('no change');
     }
     else if (x < 0) {
-        console.log('removed',x, 'parts');
+        console.log('removed', x, 'parts');
 
     }
 }
@@ -68,8 +68,8 @@ for (let x of supplyChanges) {
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:'); //
 let sum = 0
-for( ticker = 0; ticker<supplyChanges.length; ticker++) {
-  sum += supplyChanges[ticker]
+for (ticker = 0; ticker < supplyChanges.length; ticker++) {
+    sum += supplyChanges[ticker]
 }
 console.log(sum)
 
@@ -84,3 +84,18 @@ console.log(sum)
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
 console.log('9. Filling boxes with a "while" loop');
+let parts = 572//this is how many parts are on the table
+
+let counter = 0//////thing that counts how many loops are run(boxes filled)
+
+while (parts > 7) {//////while the number of parts on the table is greater than 7, run the below command.
+    parts -= 7; counter++;
+}
+
+console.log('this is how many parts i have left:', parts,);
+console.log('this is how many boxes are filled:', counter);
+
+
+
+
+
