676315ed6448987a9a960a4c38118c531505c695
Steven Gangl <steven.h.gangl@gmail.com>
2023-02-05 14:01:34 -0600

    first stretch done

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -51,9 +51,27 @@ console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
 console.log('7. Showing supplyChanges with "for of" loop');
 
+for (let x of supplyChanges) {
+    if (x > 0) {
+        console.log('added',x, 'parts');
+    }
+    else if (x === 0) {
+        console.log('no change');
+    }
+    else if (x < 0) {
+        console.log('removed',x, 'parts');
+
+    }
+}
+
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
+
+
+
+
+
 
 // 9. We have a large stash of parts in our warehouse that we 
 //    need to box up and get ready for shipment. 
